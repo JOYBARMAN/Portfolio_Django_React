6b4928fc76ba792cd42731c0a50ea7e0b08d04fd
JOYBARMAN <52423251+JOYBARMAN@users.noreply.github.com>
2022-09-28 17:32:52 +0600

    Testimonials & brand section

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -1,10 +1,101 @@
-import React from 'react'
-import './Testimonial.scss'
+import React, { useState, useEffect } from 'react';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
+import axios from 'axios';
+import { domain } from '../../env';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import './Testimonial.scss';
 
 const Testimonial = () => {
-  return (
-    <div>Testimonial</div>
-  )
-}
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [testimonials, setTestimonials] = useState([]);
+  const [brands, setBrands] = useState([]);
 
-export default Testimonial
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  // useEffect(() => {
+  //   const query = '*[_type == "testimonials"]';
+  //   const brandsQuery = '*[_type == "brands"]';
+
+  //   client.fetch(query).then((data) => {
+  //     setTestimonials(data);
+  //   });
+
+  //   client.fetch(brandsQuery).then((data) => {
+  //     setBrands(data);
+  //   });
+  // }, []);
+
+  const getTestimonial = () => {
+    axios.get(`${domain}/testimonial/`)
+      .then((response) => {
+        const data = response.data
+        setTestimonials(data);
+      })
+      .catch(error => console.error(`Error : ${error}`))
+  }
+
+  const getBrand = () => {
+    axios.get(`${domain}/brand/`)
+      .then((response) => {
+        const data = response.data
+        setBrands(data);
+      })
+      .catch(error => console.error(`Error : ${error}`))
+  }
+
+  useEffect(() => {
+    getTestimonial()
+    getBrand()
+  }, [])
+
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img src={testimonials[currentIndex].image} alt={testimonials[currentIndex].name} />
+            <div className="app__testimonial-content">
+              <p className="p-text">{testimonials[currentIndex].feedback}</p>
+              <div>
+                <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
+                <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="app__testimonial-btns app__flex">
+            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+
+            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className="app__testimonial-brands app__flex">
+        {brands.map((brand) => (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            key={brand._id}
+          >
+            <img src={brand.image} alt={brand.name} />
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  'app__primarybg',
+);
